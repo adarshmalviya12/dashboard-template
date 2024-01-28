41010d46df31fb9e34b6afdd4268697abcdd5b21
Adarsh Malviya <adarshmalviya1199@gmail.com>
2024-01-28 12:17:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { Suspense, useState } from "react";
+import "./App.css";
+import DefaultLayout from "./layout/DefaultLayout";
+import { Route, Routes } from "react-router-dom";
+import SignIn from "./pages/Authentication/SignIn";
+import SignUp from "./pages/Authentication/SignUp";
+import routes from "./routes";
+import Loader from "./common";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Routes>
+      <Route path="/auth/signin" element={<SignIn />} />
+      <Route path="/auth/signup" element={<SignUp />} />
+      <Route element={<DefaultLayout />}>
+        {routes.map((routes, index) => {
+          const { path, component: Component } = routes;
+          return (
+            <Route
+              key={index}
+              path={path}
+              element={
+                <Suspense fallback={<Loader />}>
+                  <Component />
+                </Suspense>
+              }
+            />
+          );
+        })}
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
