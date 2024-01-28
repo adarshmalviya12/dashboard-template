c77e783d35adcf38dd22f21ac660455d12cd068c
Adarsh Malviya <adarshmalviya1199@gmail.com>
2024-01-28 14:58:43 +0530

    tables

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,18 @@
 import { lazy } from "react";
 
 const Profile = lazy(() => import("../pages/Profile"));
+const Tables = lazy(() => import("../pages/Tables"));
 
 const coreRoutes = [
   {
     path: "/profile",
     title: "Profile",
     component: Profile,
+  },
+  {
+    path: "/tables",
+    title: "Tables",
+    component: Tables,
   },
 ];
 
